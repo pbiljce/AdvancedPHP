52906420c1ea100ae992e54eacc25b07b84244a0
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-12-26 18:08:54 +0100

    DateDifference

---
+++ webChallenge1.php
@@ -0,0 +1,32 @@
+<?php
+    function dateDifference($date1,$date2,$format){
+        if(empty($date2)){
+            $date2 = Date("d.m.Y");
+        }
+
+        $date1 = new DateTime($date1);
+        $date2 = new DateTime($date2);
+
+        $dateDiff = $date1 -> diff($date2); //https://www.php.net/manual/en/datetime.diff.php (PROPERTIES - y-number of years, m-number of months, d-number of days, h-number of hours, i--number of minutes, s-number of seconds, days-If the DateInterval object was created by DateTime::diff() then this is the total number of days between the start and end dates. Otherwise, days will be FALSE
+        $difference = $dateDiff -> days;
+        $differenceYears = $dateDiff -> y;
+        $differenceMonths = $dateDiff -> m + $differenceYears * 12;
+
+        switch($format){
+            case "days":
+                echo "Ukupna razlika je " . $difference . " dana.";
+            break;
+            case "months":
+                echo "Razlika u mjesecima je " . $differenceMonths . " mjeseci.";
+            break;
+            case "years":
+                echo "Razlika u godinama je " . $differenceYears . " godina.";
+            break;
+            default;
+                echo $dateDiff -> format('Razlika je %d dana %m mjeseci i %y godina');
+            break;
+        }
+    }
+
+    dateDifference("10.10.1950","12.12.2012","");
+?>
