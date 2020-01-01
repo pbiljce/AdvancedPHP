813d7d764e2382b72d469b368e03c4a8d9485020
pbiljce <57287222+pbiljce@users.noreply.github.com>
2020-01-01 18:58:31 +0100

    Kreiranje klase

---
+++ webChallenge2.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * ZADATAK - Kreiranje klase
+     * Na osnovu kreirane funkcije kreirati klasu koja izračunava razliku između dva proslijeđena datuma. 
+     * Za početak kreirati samo definiciju klase (ključna riječ class + NazivKlase)
+     */
+    class DateDifference{
+
+    }
+?>
