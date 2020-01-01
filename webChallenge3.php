5ae94856cdb047bc57b36a9d9d4f74407e4de864
pbiljce <57287222+pbiljce@users.noreply.github.com>
2020-01-01 18:58:49 +0100

    Klasa, svojstva

---
+++ webChallenge3.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * ZADATAK - Klasa, svojstva
+     * Klasu DateDifference koja izračunava razliku između dva proslijeđena datuma proširiti sa svojstvima koje će u sebe primati parametre kasnije.
+     * Ako je drugi datum neproslijeđen, uzimamo trenutni datum.
+     * Treći parametar iskoristiti za željeni ispis, odnosno da li će razlika biti ispisana u danima, mjesecima i godinama, ili u nekoj drugoj kombinaciji
+     */
+    class DateDifference{
+        public $date1 = "25.03.1980";
+        public $date2 = "01.01.2020";
+        public $format = "months";
+    }
+?>
