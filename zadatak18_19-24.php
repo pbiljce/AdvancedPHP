85931c541eeb9eaf0a108581518b8f04d63b049c
pbiljce <57287222+pbiljce@users.noreply.github.com>
2020-01-25 21:03:55 +0100

    Create zadatak18_19-24.php

---
+++ zadatak18_19-24.php
@@ -0,0 +1,41 @@
+<?php
+    /**
+     *Kreirati klasu Connection koja se konektuje na bazu i vraća konekcioni objekat
+     *Jednu verziju klase Connection uraditi sa PDO, drugu verziju uraditi sa MySQLi objektnim pristupom
+     */
+    class Connection{
+        public $servername = "localhost";
+        public $username = "root";
+        public $password = "";
+        public $database = "users";
+        public $connection;
+    
+        //MySQLi objektni pristup
+        public function connectMySQLi(){
+            $this->connection = new mysqli($this->servername,$this->username,$this->password,$this->database);
+            if($this->connection->connect_error){
+                die("Konekcija nije uspjela - " . $this->connection->connect_error);
+            }
+            else{
+                echo "Konekcija je uspostavljena";
+            }
+        }
+
+        //PDO
+        public function connectPDO(){
+            try{
+                $this->connection = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->database,$this->username,$this->password);
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                echo "Konekcija je uspostavljena";
+            }
+            catch(PDOException $e){
+                echo "Konekcija nije uspjela" . $e->getMessage();
+            }
+        }
+    }
+
+    $baza = new Connection();
+    $baza->connectMySQLi();
+    echo "<br>";
+    $baza->connectPDO();
+?>
